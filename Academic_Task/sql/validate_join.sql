da22edebde018ff19004e2983e8fcc27cfc1877c
M Saad <msaad23305@gmail.com>
2026-05-17 23:53:14 +0500

    chore: remove Academic_Task folder from repository

--- Academic_Task/sql/validate_join.sql
+++
@@ -1,6 +0,0 @@
-SELECT o.id as order_id, b.name as brand_name, p.name as product_name, oi.quantity, oi."unitPricePkr" as unit_price
-FROM "Order" o
-JOIN "OrderItem" oi ON oi."orderId" = o.id
-JOIN "Product" p ON p.id = oi."productId"
-JOIN "Brand" b ON b.id = oi."brandId"
-LIMIT 5;
